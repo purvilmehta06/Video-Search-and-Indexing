3862ac51504ce2211d87fc0542c6e30c486e552c
Nishi Doshi <nishi1612@Nishis-Air.lan>
2023-11-18 03:20:38 -0800

    Moving code to one file

--- add_noise.py
+++ add_noise.py
@@ -9,14 +9,15 @@ def get_fps(input_video_path):
     clip.close()
     return fps
 
-def clip_random_10_seconds(input_video_path, output_video_path, query_video_length):
+def get_video_duration(input_video_path):
     clip = mp.VideoFileClip(input_video_path)
-    video_duration = clip.duration
-    start_time = random.uniform(0, video_duration - query_video_length)
-    print(start_time)
+    return clip.duration
+
+def clip_random_seconds(input_video_path, output_video_path, query_video_length, start_time):
+    clip = mp.VideoFileClip(input_video_path)
     clipped_clip = clip.subclip(start_time, start_time + query_video_length)
     clipped_clip.write_videofile(output_video_path, codec='libx264', audio_codec='aac')
-
+    
 def add_gaussian_noise(image):
     row, col, ch = image.shape
     gauss = np.random.normal(mean, sigma, (row, col, ch))
@@ -33,26 +34,31 @@ def add_noise_to_video(input_video_path, output_video_path):
 
 mean = 0
 sigma = 5
-count = 0
 query_video_length = 10
 
 input_video_path = 'Dataset/Videos/video1.mp4'
 initial_name = input_video_path[:-4]
-output_video_path = initial_name + str(mean) + '_' + str(sigma) + '.mp4'
+output_video_path = initial_name  + '_' + str(mean) + '_' + str(sigma) + '.mp4'
 if not os.path.exists(output_video_path):    
     add_noise_to_video(input_video_path, output_video_path)
 else:
     print("Video with mean and sigma exists")
 
+
 input_video_path = output_video_path
+video_duration = get_video_duration(input_video_path)
+start_time = random.uniform(0, video_duration - query_video_length)
+frame_number = int(start_time*30)
+
 fps = get_fps(input_video_path)
 print("FPS of output file: ", fps)
+
 if (fps == 30.0):
-    output_video_path = initial_name + str(mean) + '_' + str(sigma) + '_' + str(count) + '.mp4'
+    output_video_path = initial_name + '_' + str(mean) + '_' + str(sigma) + '_' + str(frame_number) + '.mp4'
     if not os.path.exists(output_video_path):
-        clip_random_seconds(input_video_path, output_video_path, query_video_length)
+        clip_random_seconds(input_video_path, output_video_path, query_video_length, start_time)
         print("Clip video created")
     else:
-        print("Clip video exists")
+        print("Clip video exists for frame number: ", frame_number)
 else:
     print("Not creating clip as fps is less")
