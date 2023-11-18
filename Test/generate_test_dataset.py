f2bd9a017261e403691fe920d01dba57f745afc1
Nishi Doshi <nishi1612@Nishis-Air.lan>
2023-11-18 03:47:30 -0800

    Rename file

---
+++ Test/generate_test_dataset.py
@@ -0,0 +1,83 @@
+import moviepy.editor as mp
+import random
+import os
+import numpy as np
+import sys
+import contextlib
+import io
+
+@contextlib.contextmanager
+def suppress_moviepy_output():
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()  # Redirect stdout to an in-memory buffer
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
+
+def get_video_duration(input_video_path):
+    with suppress_moviepy_output():
+        clip = mp.VideoFileClip(input_video_path)
+        return clip.duration
+
+def clip_random_seconds(input_video_path, output_video_path, query_video_length, start_time):
+    with suppress_moviepy_output():
+        clip = mp.VideoFileClip(input_video_path)
+        clipped_clip = clip.subclip(start_time, start_time + query_video_length)
+        clipped_clip.write_videofile(output_video_path, codec='libx264', audio_codec='aac')
+    
+def add_gaussian_noise(image):
+    row, col, ch = image.shape
+    gauss = np.random.normal(mean, sigma, (row, col, ch))
+    noisy = np.clip(image + gauss, 0, 255)
+    return noisy.astype(np.uint8)
+
+def process_frame(frame):
+    return add_gaussian_noise(frame)
+
+def add_noise_to_video(input_video_path, output_video_path):
+    with suppress_moviepy_output():
+        clip = mp.VideoFileClip(input_video_path)
+        processed_clip = clip.fl_image(process_frame)
+        processed_clip.write_videofile(output_video_path, codec='libx264', audio_codec='aac')
+
+mean = 0
+sigma = 5
+query_video_length = 10
+
+if (len(sys.argv) >= 4):
+    mean = sys.argv[1]
+    sigma = sys.argv[2]
+    query_video_length = sys.argv[3]
+elif (len(sys.argv) == 3):
+    mean = sys.argv[1]
+    sigma = sys.argv[2]
+elif (len(sys.argv) == 2):
+    mean = sys.argv[1]
+
+folder_path = "../Dataset/Videos"
+for video_file in os.listdir(folder_path):
+    if video_file.endswith(".mp4"):
+        
+        print("Processing video: ", video_file)
+
+        input_video_path = folder_path + "/" + video_file
+        video_name = video_file[:-4]
+        
+        video_duration = get_video_duration(input_video_path)
+        start_time = random.uniform(0, video_duration - query_video_length)
+        frame_number = int(start_time*30)
+
+        output_video_path = "../Dataset/Clips/" + video_name + '_' + str(frame_number) + '.mp4'
+
+        if not os.path.exists(output_video_path):
+            clip_random_seconds(input_video_path, output_video_path, query_video_length, start_time)
+
+        input_video_path = output_video_path
+        output_video_path = "../Dataset/NoiseQuery/" + video_name + '_' + str(mean) + '_' + str(sigma) + '_' + str(frame_number) + '.mp4'
+
+        if not os.path.exists(output_video_path):    
+            add_noise_to_video(input_video_path, output_video_path)
+            print("Clipped video with mean and sigma created")
+        else:
+            print("Clipped video with mean and sigma exists")
