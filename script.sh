39b4a9c7f4d07d176363ecece15790eb5b8bf8f3
Nishi Doshi <doshinishi1612@gmail.com>
2023-11-20 15:56:20 -0800

    Adding files for test dataset generation and output analysis (#1)

--- script.sh
+++ script.sh
@@ -1,12 +1,21 @@
+#Generate Noise Videos
+for i in {0..5}
+do
+  for j in {10..20}
+  do
+    python3 generate_test_dataset.py 0 $i $j
+  done
+done
+
 # installing all the dependencies
 echo "Installing all the dependencies..."
 mvn clean install > mvn_install.log
 echo "Dependencies installed successfully...\n"
 
 # going through all queries and running the program
-for file in ./Dataset/Queries/*.mp4
+for file in ./Dataset/NoiseQuery/*.mp4
 do
-   mvn exec:java -Dexec.args="$file ./Dataset/Queries/audios/video1_1.wav 1 5" | grep -v '\[INFO\]'
+   mvn exec:java -Dexec.args="$file ./Dataset/Queries/audios/video1_1.wav 0 10 1" | grep -v '\[INFO\]'
    echo "\n"
 done
 
