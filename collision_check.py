81c2c7d3036b46470230999569ffc5a4299c7f50
Nishi Doshi <nishi1612@Nishis-Air.lan>
2023-12-01 21:38:09 -0800

    Check for colision

---
+++ collision_check.py
@@ -0,0 +1,26 @@
+import os
+
+set_of_rgb_values = set()
+
+def add_rgb_values_to_set(file_name):
+	values = []
+	with open(file_name, 'r') as file:
+		for line in file:
+			values.append(line)
+	list_values = []
+	for value in values:
+		list_values.append(tuple([int(num) for num in value.split(' ')]))
+	six_hundred_consecutive_values = []
+	for value in list_values:
+		six_hundred_consecutive_values.append(value)
+		if (len(six_hundred_consecutive_values) == 450):
+			if tuple(six_hundred_consecutive_values) in set_of_rgb_values:
+				print("Already present in set")
+			else:
+			    set_of_rgb_values.add(tuple(six_hundred_consecutive_values))
+			    six_hundred_consecutive_values.pop(0)
+
+folder_path = "Preprocessing/rgb_sum_values/"
+for value_file in os.listdir(folder_path):
+    print(value_file)
+    add_rgb_values_to_set(folder_path + value_file)
