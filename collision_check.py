4a3b3274b723e0566b7c9d13df75d2b094cf4ec1
Nishi Doshi <nishi1612@Nishis-MacBook-Air.local>
2023-12-01 22:23:58 -0800

    Add comments

--- collision_check.py
+++ collision_check.py
@@ -1,7 +1,6 @@
 import os
 
-set_of_rgb_values = set()
-
+# Function to check for consecutive RGB sum values for 15 seconds
 def add_rgb_values_to_set(file_name):
 	values = []
 	with open(file_name, 'r') as file:
@@ -11,16 +10,18 @@ def add_rgb_values_to_set(file_name):
 	for value in values:
 		list_values.append(tuple([int(num) for num in value.split(' ')]))
 	consecutive_values = []
+	set_of_consecutive_rgb_sum_values = set()
 	for value in list_values:
 		consecutive_values.append(value)
 		if (len(consecutive_values) == 450):
-			if tuple(consecutive_values) in set_of_rgb_values:
+			if tuple(consecutive_values) in set_of_consecutive_rgb_sum_values:
 				print("Already present in set")
 			else:
 			    set_of_rgb_values.add(tuple(consecutive_values))
-			    consecutive_values.pop(0)
+			    set_of_consecutive_rgb_sum_values.pop(0)
 
-folder_path = "Preprocessing/rgb_sum_values/"
-for value_file in os.listdir(folder_path):
-    print(value_file)
-    add_rgb_values_to_set(folder_path + value_file)
+if __name__ == "__main__":
+    folder_path = "Preprocessing/rgb_sum_values/"
+    for value_file in os.listdir(folder_path):
+        print("Processing: ", value_file)
+        add_rgb_values_to_set(folder_path + value_file)
